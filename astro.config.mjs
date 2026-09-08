f765627750b0c7b1b1d56b0f7dac1ad474045f07
smilekison <24309222+smilekison@users.noreply.github.com>
2026-09-08 11:05:13 +0545

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+// import tailwind from '@astrojs/tailwind';
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
+export default defineConfig({});
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel()
-});
+// export default defineConfig({
+//   integrations: [tailwind()],
+//   output: 'server',
+//   adapter: vercel()
+// });
